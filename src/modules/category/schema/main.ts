942d6557dca83e68d06d8c6f02ef9824fd1b2cd8
MohitNishad <nishadamar989@gmail.com>
2023-12-23 18:35:19 +0530

    use query client to render category  price sections

--- src/modules/category/schema/main.ts
+++ src/modules/category/schema/main.ts
@@ -15,11 +15,7 @@ export const CategoryPriceSectionSchema = z.object({
     sections: z.array(
         z.object({
             id: z.string(),
-<<<<<<< HEAD
             name: z
-=======
-            title: z
->>>>>>> origin/main
                 .string()
                 .min(2)
                 .transform((data) => data.toUpperCase()),
