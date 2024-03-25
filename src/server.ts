8302dddb29b1a23d67d8616595be7f2c6f3f7d32
SinghMohit <nishadamar989@gmail.com>
2024-03-25 19:30:50 +0530

    fix redis timeout error

--- src/server.ts
+++ src/server.ts
@@ -70,7 +70,7 @@ applySpeedGooseCacheLayer(mongoose, {
     redisOptions: {
         host: process.env.REDIS_HOST,
         password: process.env.REDIS_PASSWORD,
-        port: parseInt(process.env.REDIS_PASSWORD!),
+        port: parseInt(process.env.REDIS_PORT!),
     },
     defaultTtl: 100,
     debugConfig: {
